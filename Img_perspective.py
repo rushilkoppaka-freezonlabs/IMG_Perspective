810862200bc5b9c2737c9aa5608e06314c755407
rushilkoppaka-freezonlabs <83945787+rushilkoppaka-freezonlabs@users.noreply.github.com>
2021-07-31 16:52:14 +0530

    Update Img_perspective.py

--- Img_perspective.py
+++ Img_perspective.py
@@ -3,15 +3,15 @@ import cv2
 import os
 
 
-with os.scandir('Dataset2/') as entries:
+with os.scandir('../obj/') as entries:
     for entry in entries:
         print(entry.name)
-        img = cv2.imread('Dataset2/'+str(entry.name))
+        if entry.name.split('.')[-1] == 'txt':
+            continue
+        img = cv2.imread('../obj/'+str(entry.name))
         img = Perspective_image.process_img_scanned(img)
-        #cv2.imshow('win', img)
-        os.chdir('Dataset3')
+        os.chdir('../Dataset3')
         cv2.imwrite(str(entry.name), img)
-        os.chdir('..')
-        #cv2.imwrite()
-        #cv2.waitKey(0)
+        os.chdir('../IMG_Perspective')
+        
 print(os.getcwd())
